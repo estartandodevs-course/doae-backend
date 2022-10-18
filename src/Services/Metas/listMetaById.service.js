3365bcd445e2034a731b524ab5c471f618e84604
Brenda Brito <bbritob8@gmail.com>
2022-10-18 15:02:46 -0300

    atualização get metas por id

--- src/Services/Metas/listMetaById.service.js
+++ src/Services/Metas/listMetaById.service.js
@@ -1,9 +1,12 @@
 import { getMetaById } from "../../Repositories/MetaRepository.js";
+import { getProductMetasByIdMeta } from "../../Repositories/ProductMetaRepository.js";
+
 
 export async function listMetaById(id) {
     try {
         const meta = await getMetaById(id);
-        return meta;
+        const produto = await getProductMetasByIdMeta(id_meta);
+        return meta, produto;
     } catch (e) {
         console.log(e);
         throw new Error("Não foi possível buscar a meta.");
