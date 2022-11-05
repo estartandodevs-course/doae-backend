462de6c3905c9badd4ca500bce930a61751ca604
Brenda Brito <bbritob8@gmail.com>
2022-11-05 10:10:47 -0300

    create listProductByIdInstitution

---
+++ src/Services/Products/listProductByIdInstitution.service.js
@@ -0,0 +1,17 @@
+import { getProductsByIdInstitution } from "../../Repositories/ProductRepository.js";
+
+export async function listProductByIdInstitutionService(id_institution, page = 1){
+	const perPage = 10;
+	let initPage  = (page * perPage) - perPage;
+	try {
+		const products = await getProductsByIdInstitution(id_institution);
+		const response  = products.slice(initPage, (initPage + perPage));
+		if (response.length > 0) {
+			return response;
+		} else {
+			return products.slice(0, 10);
+		}
+	} catch (e) {
+		throw new Error(e.message);
+	}
+}
